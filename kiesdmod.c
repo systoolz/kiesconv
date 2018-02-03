e628c3e633dc44d1f4bcda103727ff9743172033
systoolz <iunnamed@yandex.ru>
2018-02-03 22:56:10 +0700

    v1.2

--- kiesdmod.c
+++ kiesdmod.c
@@ -14,6 +14,10 @@
 const static CCHAR s_key[] = "epovviwlx,dirwq;sor0-fvksz,erwog";
 // initialize vector
 const static CCHAR  s_iv[] = "afie,crywlxoetka";
+// v1.2
+const static CCHAR s_ver[] = "<?xml\0<HeaderData\0<version\0";
+const static CCHAR s_typ[] = "<?xml\0<HeaderData\0<zipType\0";
+const static CCHAR s_gzp[] = "GZip";
 
 // another code replace to drop msvcrt.dll dependency
 void *memmove(void *dst, const void *src, size_t count) {
@@ -82,8 +86,96 @@ BOOL result;
   return(result);
 }
 
+// v1.2
+DWORD CmpXMLNode(CCHAR *s, DWORD z, CCHAR *d, DWORD x) {
+DWORD r;
+CCHAR a, b;
+  r = 0;
+  if (s && d && z && x) {
+    r = x;
+    while (x < z) {
+      a = *s;
+      b = *d;
+      a |= ((a >= 'A') && (a <= 'Z')) ? 0x20 : 0;
+      b |= ((b >= 'A') && (b <= 'Z')) ? 0x20 : 0;
+      if (a != b) { break; }
+      s++;
+      z--;
+      d++;
+      x--;
+    }
+    if ((!x) && (z) && ((*s == '>') || (*s == ' ') || (*s == '\t') || (*s == '\n') || (*s == '\r'))) {
+      while ((z) && (*s != '>')) {
+        s++;
+        z--;
+        r++;
+      }
+      if ((z) && (*s == '>')) { r++; }
+    } else {
+      r = 0;
+    }
+  }
+  return(r);
+}
+
+// v1.2
+void GetXMLValue(CCHAR *s, DWORD z, CCHAR *f, CCHAR *v, DWORD l) {
+DWORD r, x;
+  if (v && l) {
+    *v = 0;
+    if (s && z && f) {
+      x = lstrlenA(f);
+      while (z > x) {
+        r = CmpXMLNode(s, z, f, x);
+        if (r) {
+          f += x + 1;
+          s += r;
+          z -= r;
+          if (*f) {
+            GetXMLValue(s, z, f, v, l);
+          } else {
+            l = (z < l) ? z : l;
+            if (l) {
+              CopyMemory(v, s, l * sizeof(v[0]));
+              v[l - 1] = 0;
+              for (l = 0; v[l]; l++) {
+                if (v[l] == '<') {
+                  v[l] = 0;
+                  break;
+                }
+              }
+            }
+          }
+          break;
+        }
+        s++;
+        z--;
+      }
+    }
+  }
+}
+
+BOOL DumpToFile(TCHAR *filename, void *p, DWORD sz) {
+HANDLE fl;
+DWORD dw;
+BOOL r;
+  r = FALSE;
+  if (filename) {
+    fl = CreateFile(filename, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, 0);
+    if (fl != INVALID_HANDLE_VALUE) {
+      if (p && sz) {
+        WriteFile(fl, p, sz, &dw, NULL);
+      }
+      CloseHandle(fl);
+      r = TRUE;
+    }
+  }
+  return(r);
+}
+
 DWORD DecodeKiesFile(TCHAR *cryptfile, TCHAR *plainfile) {
 BYTE *p, *u;
+CCHAR v[8];
 DWORD i, sz, r;
 HANDLE fl;
   r = 1; // cant open
@@ -110,18 +202,29 @@ HANDLE fl;
       for (i = 1; i < sz; i++) {
         MoveMemory(&p[i*CRYPT_BLOCK_RAW], &p[i*CRYPT_BLOCK_LEN], CRYPT_BLOCK_RAW);
       }
-      r = 5; // unpacking failed
-      sz -= 2;
-      u = GZUnpack(&p[CRYPT_BLOCK_RAW*2], sz * CRYPT_BLOCK_RAW);
-      if (u) {
-        r = 6; // cant create
-        fl = CreateFile(plainfile, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, 0);
-        if (fl != INVALID_HANDLE_VALUE) {
-          WriteFile(fl, &u[4], *((DWORD *) u), &i, NULL);
-          CloseHandle(fl);
-          r = 0; // let's hope it was alright
+      // v1.2
+      // check XML version
+      GetXMLValue((CCHAR*) p, CRYPT_BLOCK_RAW, (CCHAR *) s_ver, v, 8);
+      // .SPB 1.x: "1.x"; .SPB 2.x: "2.x"; .SSC 2.x: "Version:2.0"
+      if (v[0]) {
+        // calculate version and header size
+        i = (v[0] == '1') ? 1 : 2;
+        // blocks
+        sz -= i;
+        // check if unpacking required
+        GetXMLValue((CCHAR*) p, CRYPT_BLOCK_RAW*i, (CCHAR *) s_typ, v, 8);
+        if (!lstrcmpiA(v, (CCHAR *) s_gzp)) {
+          r = 5; // unpacking failed
+          u = GZUnpack(&p[CRYPT_BLOCK_RAW*i], sz * CRYPT_BLOCK_RAW);
+          if (u) {
+            // cant create
+            r = DumpToFile(plainfile, &u[4], *((DWORD *) u)) ? 0 : 6;
+            FreeMem(u);
+          }
+        } else {
+          // probably plain .XML file
+          r = DumpToFile(plainfile, &p[CRYPT_BLOCK_RAW*i], sz * CRYPT_BLOCK_RAW) ? 0 : 6;
         }
-        FreeMem(u);
       }
     }
     FreeMem(p);
